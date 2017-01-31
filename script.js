f3b34cf1310b83bb1cb3302b84c5d68682aa4232
MICHAEL ROBERTS JR <michaelrobertsjr@MICHAELs-MacBook-Pro.local>
2017-01-30 20:28:51 -0800

    adds initial files

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var names = [];
+var keyName = 'first';
+
+names.push({first: 'mike'});
+
+console.log(names[0].first);
+console.log(names[0][keyName]);
+
+var myValue = (x == 'yes' ) ? true : false;
+
+if(x == 'yes') {
+  // not a lot of code
+  return true;
+} else {
+  return false;
+}
